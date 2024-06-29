c1fb5ec2b1a3c7a882558caa76b898d94142b5db
Jörg Bayreuther <jb@visualjerk.de>
2024-06-29 08:42:06 +0200

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+import { minimal2023Preset as preset } from '@vite-pwa/assets-generator/config'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'My Awesome App',
+        short_name: 'MyApp',
+        description: 'My Awesome App description',
+      },
+      pwaAssets: {
+        preset,
+        image: 'public/icon.svg',
+      },
+    }),
+  ],
   base: '/swu-health-counter/',
 })
