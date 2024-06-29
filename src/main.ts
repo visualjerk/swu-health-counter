3b5d0968f3bca71d61ab5f92c0121bc77987243a
Jörg Bayreuther <jb@visualjerk.de>
2024-06-29 08:56:24 +0200

    add wake lock

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,13 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+async function startWakeLock() {
+  try {
+    await navigator.wakeLock.request()
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+startWakeLock()
 createApp(App).mount('#app')
